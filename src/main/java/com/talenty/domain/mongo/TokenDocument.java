94225d8b5a59f34e6db8548d1331d0d3c3dd602c
unknown <rafayel.shahnazaryan@gmail.com>
2021-11-24 17:41:19 +0400

    few changes

--- src/main/java/com/talenty/domain/mongo/TokenDocument.java
+++ src/main/java/com/talenty/domain/mongo/TokenDocument.java
@@ -1,17 +1,25 @@
 package com.talenty.domain.mongo;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @Document("tokens")
 public class TokenDocument {
 
+    @Id
+    private String id;
+    @Indexed(unique = true)
     private String value;
     private String userId;
+
+    public TokenDocument(final String value, final String userId) {
+        this.value = value;
+        this.userId = userId;
+    }
 
 }
