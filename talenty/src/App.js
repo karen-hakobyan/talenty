d4ddb8145a4cf89fd1b127e2ab90b370652bdad9
Artur <tov.art@bk.ru>
2021-12-02 22:07:32 +0400

    Sign up changes

--- talenty/src/App.js
+++ talenty/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import CvTemplate from "./components/CvTemplate/CvTemplate";
+
+import SignIn from "./components/SiginIn/SignIn";
 
 function App() {
   return (
     <>
-      <CvTemplate />
+      <SignIn />
     </>
   );
 }
