d350826a93c6fc903419bfdc5c691110365c5748
Karen Hakobyan <karenh.akobyan95real@gmail.com>
2022-01-11 20:20:43 +0400

    country validation and some changed added

--- src/main/java/com/talenty/validation/ValidationChecker.java
+++ src/main/java/com/talenty/validation/ValidationChecker.java
@@ -9,12 +9,12 @@ import com.twilio.exception.ApiException;
 import com.twilio.rest.lookups.v1.PhoneNumber;
 
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class ValidationChecker {
 
+    private static final List<String> COUNTRIES = new ArrayList<>(Arrays.asList("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe", "Palestine"));
     private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
     private static final Pattern COMPANY_NAME_REGEX = Pattern.compile("^[a-zA-Z]([a-zA-Z0-9.-_,]|[- @.#&!])*$");
     private static final Pattern NAME_REGEX = Pattern.compile("^[A-Z][a-z]*(([,.] |[ '-])[A-Za-z][a-z]*)*(\\.?)( [IVXLCDM]+)?$");
@@ -28,9 +28,9 @@ public class ValidationChecker {
     }
 
     public static boolean assertDetailsAreValid(final HrRegisterRequestDetails details) {
-        return isEmailValid(details.getEmail()) &&
-                isCompanyNameValid(details.getCompanyName()) &&
-                isNameValid(
+        return assertEmailIsValid(details.getEmail()) &&
+                assertCompanyNameIsValid(details.getCompanyName()) &&
+                assertNameIsValid(
                         details.getFirstName().replace(" ", "")
                                 + " "
                                 + details.getLastName().replace(" ", "")
@@ -40,8 +40,8 @@ public class ValidationChecker {
     }
 
     public static boolean assertDetailsAreValid(final JobSeekerRegisterRequestDetails details) {
-        return isEmailValid(details.getEmail()) &&
-                isNameValid(
+        return assertEmailIsValid(details.getEmail()) &&
+                assertNameIsValid(
                         details.getFirstName().replace(" ", "")
                                 + " "
                                 + details.getLastName().replace(" ", "")
@@ -75,7 +75,7 @@ public class ValidationChecker {
 
             case "email": {
                 assertLengthIsValid(submittedField, parentField);
-                isEmailValid(submittedValue);
+                assertEmailIsValid(submittedValue);
                 break;
             }
 
@@ -118,11 +118,15 @@ public class ValidationChecker {
                 break;
             }
 
-            case "special_name":
+            case "city":
+            case "special_name": {
                 assertLengthIsValid(submittedField, parentField);
-            case "country":
-            case "city": {
-                isNameValid(submittedValue);
+                assertNameIsValid(submittedValue);
+                break;
+            }
+
+            case "country": {
+                assertCountryIsValid(submittedValue);
                 break;
             }
 
@@ -173,7 +177,7 @@ public class ValidationChecker {
         return true;
     }
 
-    private static boolean isEmailValid(final String email) {
+    private static boolean assertEmailIsValid(final String email) {
         if (!EMAIL_REGEX.matcher(email).matches()) {
             System.out.println("Email format exception!");
             throw new InvalidEmailException();
@@ -181,7 +185,7 @@ public class ValidationChecker {
         return true;
     }
 
-    private static boolean isCompanyNameValid(final String companyName) {
+    private static boolean assertCompanyNameIsValid(final String companyName) {
         if (!COMPANY_NAME_REGEX.matcher(companyName).matches()) {
             System.out.println("Company name format exception!");
             throw new InvalidCompanyNameException();
@@ -189,7 +193,7 @@ public class ValidationChecker {
         return true;
     }
 
-    private static boolean isNameValid(final String name) {
+    private static boolean assertNameIsValid(final String name) {
         if (!NAME_REGEX.matcher(name).matches()) {
             System.out.println("Name format exception!");
             throw new InvalidUserNameException();
@@ -197,21 +201,27 @@ public class ValidationChecker {
         return true;
     }
 
-    public static boolean assertPasswordIsValid(final String password) {
+    private static boolean assertCountryIsValid(final String country) {
+        if (!COUNTRIES.contains(country))
+            throw new InvalidCountryException();
+        return true;
+    }
+
+    private static boolean assertPasswordIsValid(final String password) {
         if (!PASSWORD_REGEX.matcher(password).matches()) {
             throw new InvalidPasswordException();
         }
         return true;
     }
 
-    public static boolean assertPasswordsAreEqual(final String password, final String confirmPassword) {
+    private static boolean assertPasswordsAreEqual(final String password, final String confirmPassword) {
         if (!Objects.equals(password, confirmPassword)) {
             throw new PasswordsDoNotMatchException();
         }
         return true;
     }
 
-    public static boolean assertPhoneNumberIsValid(final String phoneNumber) {
+    private static boolean assertPhoneNumberIsValid(final String phoneNumber) {
         if (!PHONE_NUMBER_REGEX.matcher(phoneNumber).matches()) {
             System.out.println("Incorrect phone number format!");
             throw new InvalidPhoneNumberException();
