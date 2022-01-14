038cf4b50053df59dcfd54fba7a9f4c20bd0aa29
Andranik <andtsatur1296@gmail.com>
2022-01-14 14:04:03 +0400

    example shared button sx prop

--- talenty/src/assets/icons/createTemplate/index.jsx
+++ talenty/src/assets/icons/createTemplate/index.jsx
@@ -1,21 +1,23 @@
 import { ReactComponent as EditSVG } from "./edit.svg";
-import { ReactComponent as ListSVG } from './list.svg'
+import { ReactComponent as ListSVG } from "./list.svg";
 import { ReactComponent as EditBtnSVG } from "./editBtn.svg";
 import { ReactComponent as DisabledDeleteBtnSVG } from "./disabledDeleteBtn.svg";
 import { ReactComponent as DeleteBtn } from "./deleteBtn.svg";
 import { ReactComponent as AddSection } from "./addSection.svg";
 import { ReactComponent as CreateCV } from "./createCV.svg";
 import { ReactComponent as CreateCVDisabled } from "./createCVDisabled.svg";
-import { ReactComponent as AddSectionIconSVG } from './addSectionIcon.svg'
+import { ReactComponent as AddSectionIconSVG } from "./addSectionIcon.svg";
+import { ReactComponent as DeleteIconSVG } from "./delete.svg";
 
 export {
-    EditSVG,
-    DeleteBtn,
-    EditBtnSVG,
-    DisabledDeleteBtnSVG,
-    ListSVG,
-    AddSection,
-    CreateCV,
-    CreateCVDisabled,
-    AddSectionIconSVG
+  EditSVG,
+  DeleteBtn,
+  EditBtnSVG,
+  DisabledDeleteBtnSVG,
+  ListSVG,
+  AddSection,
+  CreateCV,
+  CreateCVDisabled,
+  AddSectionIconSVG,
+  DeleteIconSVG,
 };
--- talenty/src/components/CvTemplate/TemplateItem.jsx
+++ talenty/src/components/CvTemplate/TemplateItem.jsx
@@ -1,22 +1,36 @@
 import { memo } from "react";
-import { ListItem, ListItemText } from "@mui/material";
-import {
-  EditBtnSVG,
-  DisabledDeleteBtnSVG,
-  DeleteBtn,
-} from "../../assets/icons/createTemplate";
+import { Box, IconButton, ListItem, ListItemText } from "@mui/material";
+import { EditSVG, DeleteIconSVG } from "../../assets/icons/createTemplate";
 import { ListItemStyle } from "./CVTemplateStyle";
+import {
+  TEMPLATE_ITEM_BUTTON,
+  TEMPLATE_ITEM_BUTTON_DISABLED,
+} from "../../shared/styles";
+import { ACTION_WRAPPER } from "./style";
 
 function TemplateItem({ onDialogOpen, item }) {
   return (
     <ListItem sx={ListItemStyle} divider>
       <ListItemText primary={item.name} />
-      <EditBtnSVG onClick={() => onDialogOpen(item)} />
-      {item.metadata.deletable ? (
-        <DeleteBtn />
-      ) : (
-        <DisabledDeleteBtnSVG />
-      )}
+      <Box sx={ACTION_WRAPPER}>
+        <IconButton
+          onClick={() => onDialogOpen(item)}
+          sx={TEMPLATE_ITEM_BUTTON}
+        >
+          <EditSVG />
+          Edit
+        </IconButton>
+        <IconButton
+          sx={
+            item.metadata.deletable
+              ? TEMPLATE_ITEM_BUTTON
+              : TEMPLATE_ITEM_BUTTON_DISABLED
+          }
+        >
+          <DeleteIconSVG />
+          Delete
+        </IconButton>
+      </Box>
     </ListItem>
   );
 }
