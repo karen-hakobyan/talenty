49d16a4d9d4641b967d4a59ec16c3caeaa208809
rshakhnazari <rafayel.shahnazaryan@gmail.com>
2022-01-14 15:57:13 +0400

    exceptions essages class removed, added messaged.properties

--- src/main/java/com/talenty/exceptions/EmailAlreadyRegisteredException.java
+++ src/main/java/com/talenty/exceptions/EmailAlreadyRegisteredException.java
@@ -1,10 +1,9 @@
 package com.talenty.exceptions;
 
-import com.talenty.exceptions.messages.TalentyExceptionMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = TalentyExceptionMessage.EMAIL_ALREADY_REGISTERED)
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "exception.response.status.EMAIL_ALREADY_REGISTERED")
 public class EmailAlreadyRegisteredException extends RuntimeException {
 
     public EmailAlreadyRegisteredException(final String message) {
--- src/main/java/com/talenty/exceptions/messages/TalentyExceptionMessage.java
+++
@@ -1,5 +0,0 @@
-package com.talenty.exceptions.messages;
-
-public class TalentyExceptionMessage {
-    public static final String EMAIL_ALREADY_REGISTERED = "EMAIL_ALREADY_REGISTERED";
-}
