47ad455ba3598759dc9af26d628e9cf80c2536d6
Andranik <andtsatur1296@gmail.com>
2022-01-20 11:00:03 +0400

    delete some stuff

--- talenty/src/components/CvTemplate/CvTemplateMain.jsx
+++ talenty/src/components/CvTemplate/CvTemplateMain.jsx
@@ -1,33 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  Container,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  Skeleton,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Container, IconButton, Typography } from "@mui/material";
 import { PINK } from "../../constants/colors";
 import {
   AddSectionIconSVG,
   ListSVG,
-  EditSVG,
   CreateCV,
-  CreateCVDisabled,
 } from "../../assets/icons/createTemplate";
 import { Box } from "@mui/system";
-import { ListStyle, TextFieldStyle, StyledBtns } from "./CVTemplateStyle";
+import { StyledBtns } from "./CVTemplateStyle";
 import TemplateItem from "./TemplateItem";
 import { GET_TEMPLATES } from "../../constants/requests";
 import hrExData from "../../helpers/ajabsandal";
 
 function CvTemplateMain() {
   const [data, setData] = useState(null);
-  const [tempName, setTempName] = useState("CV Template name");
-  const [toggle, setToggle] = useState(true);
 
   useEffect(() => {
     axios
@@ -46,6 +33,7 @@ function CvTemplateMain() {
   if (!data) {
     return null;
   }
+
   return (
     <Container>
       <Box sx={{ display: "flex", mt: 5 }}>
@@ -62,41 +50,9 @@ function CvTemplateMain() {
           Create CV Template
         </Typography>
       </Box>
-      <List sx={ListStyle} component="nav" aria-label="templates">
-        {toggle ? (
-          <ListItem sx={{ pl: 0, pb: 0 }} button divider>
-            <ListItemText
-              onClick={() => setToggle(false)}
-              primary={tempName}
-              sx={{ cursor: "text" }}
-              onChange={(e) => setTempName(e.target.value)}
-            />
-            <EditSVG />
-          </ListItem>
-        ) : (
-          <TextField
-            sx={TextFieldStyle}
-            fullWidth
-            onBlur={() => tempName && setToggle(true)}
-            error={true}
-            helperText={!tempName && "You should have name for the Template"}
-            value={tempName}
-            onChange={(e) => setTempName(e.target.value)}
-            variant="standard"
-          />
-        )}
-        {Object.keys(data).length ? (
-          data.fields.map((item) => (
-            <TemplateItem key={item._id} item={item} setData={setData} />
-          ))
-        ) : (
-          <>
-            <Skeleton sx={{ mt: 3 }} animation="wave" />
-            <Skeleton animation="wave" />
-            <Skeleton animation="wave" />
-          </>
-        )}
-      </List>
+      {data.fields.map((item) => (
+        <TemplateItem key={item._id} item={item} setData={setData} />
+      ))}
       <Box sx={StyledBtns}>
         <IconButton
           sx={{
@@ -114,13 +70,9 @@ function CvTemplateMain() {
           <AddSectionIconSVG />
           Add section
         </IconButton>
-        {true ? (
-          <IconButton>
-            <CreateCV />
-          </IconButton>
-        ) : (
-          <CreateCVDisabled />
-        )}
+        <IconButton>
+          <CreateCV />
+        </IconButton>
       </Box>
     </Container>
   );
