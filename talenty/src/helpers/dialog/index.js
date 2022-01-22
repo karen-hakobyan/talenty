98d3b3fd5eebb0743c89db42e0e2a3fa26cd1474
Andranik <andtsatur1296@gmail.com>
2022-01-22 15:15:39 +0400

    fix deletiion issue

--- talenty/src/helpers/dialog/index.js
+++ talenty/src/helpers/dialog/index.js
@@ -1,9 +1,9 @@
 import { setDialogData } from "../../store/dialogs/slice";
 
-export function onDelete({ dispatch, id, dialogData }) {
+export function onDelete({ dispatch, name, dialogData }) {
     const updatedDialogData = {
         ...dialogData,
-        fields: dialogData.fields.filter((el) => el.id !== id),
+        fields: dialogData.fields.filter((el) => el.name !== name),
     };
     dispatch(setDialogData(updatedDialogData));
 }
@@ -11,4 +11,22 @@ export function isDisabled({ templateData, value }) {
     return (
         templateData.fields.some((el) => el.name === value) || value.length === 0
     );
+}
+
+export function editCheckboxState({ dispatch, dialogData, name: initialName }) {
+    const updatedDialogData = {
+        ...dialogData,
+        fields: dialogData.fields.map((field) => {
+            const { name, metadata } = field;
+            console.log(name);
+            if (name === initialName) {
+                return {
+                    ...field,
+                    metadata: {...metadata, required: !metadata.required },
+                };
+            }
+            return field;
+        }),
+    };
+    dispatch(setDialogData(updatedDialogData));
 }
