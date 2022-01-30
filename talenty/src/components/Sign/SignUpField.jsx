d7d5dab583efea0200e39913804e0ea96db2bd05
Andranik <andtsatur1296@gmail.com>
2022-01-30 14:53:48 +0400

    add validation

--- talenty/src/components/Sign/SignUpField.jsx
+++ talenty/src/components/Sign/SignUpField.jsx
@@ -2,7 +2,8 @@ import { Box, TextField } from "@mui/material";
 import React from "react";
 
 const SignUpField = React.forwardRef(
-  ({ register, value, isPassword, objKey }, ref) => {
+  ({ register, value, isPassword, objKey, errors, error }, ref) => {
+    console.log(errors);
     return (
       <Box
         sx={{ display: "flex", gap: "10px", flexDirection: "column" }}
@@ -12,8 +13,10 @@ const SignUpField = React.forwardRef(
         <TextField
           {...(isPassword ? { type: "password" } : {})}
           sx={{ width: "466px" }}
-          {...register(objKey)}
+          {...register(objKey, error)}
           placeholder={value}
+          error={!!errors?.[objKey]}
+          helperText={errors[objKey] ? errors[objKey].message : ""}
         />
       </Box>
     );
--- talenty/src/components/Sign/helper.js
+++ talenty/src/components/Sign/helper.js
@@ -1,27 +1,78 @@
-export const FIELDS = [{
+export const FIELDS = (watch) => [{
         name: "First name",
         key: "firstName",
+        error: {
+            required: "This field is required",
+            maxLength: { value: 50, message: "max length should be 50." },
+            pattern: {
+                value: /^[A-Z]/,
+                message: "First letter should be uppercase",
+            },
+        },
     },
     {
         name: "Last name",
         key: "lastName",
+        error: {
+            maxLength: { value: 50, message: "max length should be 50." },
+            required: "This field is required",
+            pattern: {
+                value: /^[A-Z]/,
+                message: "First letter should be uppercased",
+            },
+        },
     },
     {
         name: "Email",
         key: "email",
+        error: {
+            required: "This field is required",
+            pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "The email is invalid",
+            },
+        },
     },
     {
         name: "Password",
         isPassword: true,
         key: "password",
+        error: {
+            required: "The field is Required",
+            minLength: {
+                value: 8,
+                message: "Password should contain min 8, max 12 characters",
+            },
+            maxLength: {
+                value: 12,
+                message: "Password should contain min 8, max 12 characters",
+            },
+            pattern: {
+                value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/,
+                message: "Password should contain one uppercase and symbol",
+            },
+        },
     },
     {
         name: "Confirm password",
         isPassword: true,
         key: "confirmPassword",
+        error: {
+            validate: (value) => {
+                let pass = watch("password");
+                return value === pass || "Passwords are not same";
+            },
+        },
     },
 ];
-export const FIELDS_COMPANY = [
-    { name: "Company name", key: "companyName" },
-    ...FIELDS,
+
+export const FIELDS_COMPANY = (watch) => [{
+        name: "Company name",
+        key: "companyName",
+        error: {
+            required: "This field is required",
+            maxLength: { value: 100, message: "Max length for company name is 100" },
+        },
+    },
+    ...FIELDS(watch),
 ];
