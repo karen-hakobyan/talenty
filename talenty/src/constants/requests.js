487e910943ca1e8782546c15229786bb33843487
Vahe Mkrtchyan <vahemkrtchyan@workfront.com>
2022-02-13 01:32:38 +0400

    Changed backend url env var name

--- talenty/src/constants/requests.js
+++ talenty/src/constants/requests.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.BACKEND_URL || "http://localhost:7800";
+const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:7800";
 
 export const GET_TEMPLATES = `${baseUrl}/templates/system`;
 export const POST_SIGN_UP_HR = `${baseUrl}/register/hr`;
