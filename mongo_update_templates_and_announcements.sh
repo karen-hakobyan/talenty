1d4a0a9138b97072e1741650c93c16e32faa3211
rshakhnazari <rafayel.shahnazaryan@gmail.com>
2022-03-12 22:04:44 +0400

    some changes

---
+++ mongo_update_templates_and_announcements.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set +e
+
+docker exec -it talenty-mongo bash -c "mongo talenty --eval 'db.templates.drop()'"
+docker exec -it talenty-mongo bash -c "mongo talenty --eval 'db.job_announcements.drop()'"
+
+docker cp mongo.js talenty-mongo:mongo.js
+
+docker exec talenty-mongo mongo localhost/talenty mongo.js
+
+while [ $? -ne 0 ]; do
+  sleep 1
+  docker exec talenty-mongo mongo localhost/talenty mongo.js
+done
