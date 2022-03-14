c805f998ac731d70c6bd39fdbdd32affd83a4f20
rshakhnazari <rafayel.shahnazaryan@gmail.com>
2022-03-15 00:28:13 +0400

    users templates list update

--- db/clear_user_template_data.js
+++ db/clear_user_template_data.js
@@ -1,10 +1,9 @@
-let system_template = db.templates.find({"system": true})
-let system_template_id = system_template[0]._id + ""
+let system_template = db.templates.find({"system": true})[0]
+let system_template_id = system_template._id + ""
 
 let new_templates = []
 let new_object = {}
 new_object[system_template_id] = system_template.name
-
 new_templates.push(new_object)
 
 let users = db.users.find({})
