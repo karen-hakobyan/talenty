bddc196198088a104005e9a6c400fc1979a85d14
Andranik <andtsatur1296@gmail.com>
2022-03-17 21:52:14 +0400

    changes

--- talenty/src/constants/requests.js
+++ talenty/src/constants/requests.js
@@ -7,11 +7,11 @@ export const instance = axios.create({
     baseURL: baseUrl
 })
 
-export const GET_TEMPLATES = `/templates/system`;
+export const GET_TEMPLATES = `/cv_template/system`;
 export const POST_SIGN_UP_HR = `/register/hr`;
 export const POST_SIGN_UP_JOB_SEEKER = `/register/jobseeker`;
-export const All_TEMPLATES = '/templates/all'
-export const TEMPLATE_BY_ID = (id) => `/templates/template?id=${id}`
+export const All_TEMPLATES = '/cv_template/all'
+export const TEMPLATE_BY_ID = (id) => `/cv_template?id=${id}`
 export const LOGIN = `/login`;
 export const getForgotPassword = (email) => {
     return `/reset/password?email=${email}`
