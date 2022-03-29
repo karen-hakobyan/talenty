25503f5311234dee8eb4099e5321f56bed549b8f
rshakhnazari <rafayel.shahnazaryan@gmail.com>
2022-03-29 21:07:40 +0400

    logic changed, publishing added

--- src/main/java/com/talenty/repository/JobAnnouncementRepository.java
+++ src/main/java/com/talenty/repository/JobAnnouncementRepository.java
@@ -1,12 +1,17 @@
 package com.talenty.repository;
 
 import com.talenty.domain.mongo.JobAnnouncementDocument;
+import com.talenty.enums.JobAnnouncementStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface JobAnnouncementRepository extends MongoRepository<JobAnnouncementDocument, String> {
 
     @Query(value = "{'system' : true}", fields = "{}")
     JobAnnouncementDocument findSystemJobAnnouncement();
+
+    List<JobAnnouncementDocument> findAllByStatus(JobAnnouncementStatus status);
 
 }
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -24,10 +24,22 @@ let types_list = []
 updateTypeList()
 console.log("TYPES AND LISTS: " + types_list)
 
+let job_announcements = []
+updateJobAnnouncements()
+console.log("JOB ANNOUNCEMENTS: " + job_announcements)
+
 function updateTypeList() {
     types_list = JSON.parse(httpGet(BACKEND_URL + "/type_values/get_all"))
     if (!types_list) {
         types_list = []
     }
     return types_list
+}
+
+function updateJobAnnouncements() {
+    job_announcements = JSON.parse(httpGet(BACKEND_URL + "/job_announcements/all_pending"))
+    if (!job_announcements) {
+        job_announcements = []
+    }
+    return job_announcements
 }
