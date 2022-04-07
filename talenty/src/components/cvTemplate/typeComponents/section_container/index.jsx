7fe1fd3f1b7827389fb15730eda718c9afab4427
Andranik <andtsatur1296@gmail.com>
2022-04-07 19:15:36 +0400

    fix initial bugs

--- talenty/src/components/cvTemplate/typeComponents/section_container/index.jsx
+++ talenty/src/components/cvTemplate/typeComponents/section_container/index.jsx
@@ -1,6 +1,10 @@
-import {memo} from "react";
-import {Box} from "@mui/material";
+import React, {memo} from "react";
+import {Box, IconButton} from "@mui/material";
 import typeComponents from "..";
+import SubSection from "../../../shared/subSection";
+import {TEMPLATE_ITEM_BUTTON} from "../../../../shared/styles";
+import {onDelete} from "../../../../helpers/dialog";
+import {DeleteIconSVG} from "../../../../assets/icons/createTemplate";
 
 export default function SectionContainer({
                                              data,
@@ -11,26 +15,46 @@ export default function SectionContainer({
     if (!data) {
         return null;
     }
+    const inputComponent = <Box sx={{display: "grid", gridTemplateColumns: "auto", gap: "24px", width: '100%'}}>
+        {data.fields.map((field) => {
+            if (field.metadata.status === "DELETED") {
+                return null;
+            }
+            let TempComponent = typeComponents[field.metadata.type];
+            if (!TempComponent) {
+                return <h1>they changed some type</h1>;
+            }
+            TempComponent = memo(TempComponent);
+            return (
+                <TempComponent
+                    data={field}
+                    key={field.name}
+                    isSectionContainer
+                    {...{dialogData, templateData, dispatch, sectionId: field.id}}
+                />
+            );
+        })}
+    </Box>
     return (
-        <Box sx={{display: "grid", gridTemplateColumns: "auto", gap: "24px"}}>
-            {data.fields.map((field) => {
-                if (field.metadata.status === "DELETED") {
-                    return null;
-                }
-                let TempComponent = typeComponents[field.metadata.type];
-                if (!TempComponent) {
-                    return <h1>they changed some type</h1>;
-                }
-                TempComponent = memo(TempComponent);
-                return (
-                    <TempComponent
-                        data={field}
-                        key={field.name}
-                        isSectionContainer
-                        {...{dialogData, templateData, dispatch, sectionId: field.id}}
-                    />
-                );
-            })}
-        </Box>
+        data.metadata.deletable ? <SubSection
+            inputComponent={inputComponent}
+            buttonComponent={
+                (
+                    <IconButton
+                        sx={TEMPLATE_ITEM_BUTTON}
+                        onClick={() =>
+                            onDelete({
+                                dispatch,
+                                dialogData,
+                                id: data.id,
+                            })
+                        }
+                    >
+                        <DeleteIconSVG/>
+                        Delete
+                    </IconButton>
+                )
+            }
+        /> : inputComponent
     );
 }
