d4676d03c3bdc212919b862be8f13428049a5411
Karen Hakobyan <karenh.akobyan95real@gmail.com>
2022-04-16 14:46:01 +0400

    CleanUpMetadataExecutor fixed

--- src/main/java/com/talenty/logical_executors/CleanUpMetadataExecutor.java
+++ src/main/java/com/talenty/logical_executors/CleanUpMetadataExecutor.java
@@ -1,14 +1,10 @@
 package com.talenty.logical_executors;
 
-import com.talenty.domain.dto.user.AuthenticatedUser;
 import com.talenty.domain.mongo.FieldDocument;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class CleanUpMetadataExecutor implements LogicExecutor {
 
@@ -22,22 +18,19 @@ public class CleanUpMetadataExecutor implements LogicExecutor {
 
     @Override
     public FieldDocument execute(final FieldDocument field) {
-        final AuthenticatedUser user = (AuthenticatedUser) SecurityContextHolder.getContext().getAuthentication().getCredentials();
-        if ("ROLE_JOB_SEEKER".equals(user.getRole())) {
-            final Map<String, Object> metadata = field.getMetadata();
-            if (!deleteExcept) {
-                for (final String s : fields) metadata.remove(s);
-                return field;
-            }
-
-            final List<String> anotherFields = new ArrayList<>();
-            for (final Map.Entry<String, Object> entry : metadata.entrySet()) {
-                final String key = entry.getKey();
-                if (!fields.contains(key)) anotherFields.add(key);
-            }
-            for (final String s : anotherFields) metadata.remove(s);
-
+        final Map<String, Object> metadata = field.getMetadata();
+        if (!deleteExcept) {
+            for (final String s : fields) metadata.remove(s);
+            return field;
         }
+
+        final List<String> anotherFields = new ArrayList<>();
+        for (final Map.Entry<String, Object> entry : metadata.entrySet()) {
+            final String key = entry.getKey();
+            if (!fields.contains(key)) anotherFields.add(key);
+        }
+        for (final String s : anotherFields) metadata.remove(s);
+
         return field;
     }
 
