38cb377dabded16a30c30b106dc650ac2fcc954d
Karen Hakobyan <karenh.akobyan95real@gmail.com>
2022-05-12 23:22:51 +0400

    hot fix

--- src/main/resources/static/js/consts.js
+++ src/main/resources/static/js/consts.js
@@ -1,4 +1,4 @@
-// const BACKEND_URL = "https://api.talenty.duckdns.org"
-const BACKEND_URL = "http://localhost:7800"
-// const FRONTEND_URL = "https://talenty.duckdns.org"
-const FRONTEND_URL = "http://localhost:3000"
+const BACKEND_URL = "https://api.talenty.duckdns.org"
+// const BACKEND_URL = "http://localhost:7800"
+const FRONTEND_URL = "https://talenty.duckdns.org"
+// const FRONTEND_URL = "http://localhost:3000"
