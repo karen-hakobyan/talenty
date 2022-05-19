912add0fc875ec1dceef1493ea8781299def3910
Rafael Shakhnazarian <rshakhnazari@Rafaels-MacBook-Pro.local>
2022-05-19 22:26:13 +0400

    added some scripts

--- db/update_job_announcements.sh
+++ db/update_job_announcements.sh
@@ -10,6 +10,6 @@ while [ $? -ne 0 ]; do
   docker exec talenty-mongo mongo localhost/talenty announcements_data.js
 done
 
-docker cp clear_user_cv_job_announcement_data talenty-mongo:clear_user_cv_job_announcement_data
+docker cp clear_user_job_announcement_data.js talenty-mongo:clear_user_job_announcement_data.js
 
-docker exec talenty-mongo mongo localhost/talenty clear_user_cv_job_announcement_data
+docker exec talenty-mongo mongo localhost/talenty clear_user_job_announcement_data.js
